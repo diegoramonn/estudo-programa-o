15b9f70ea9d7162e34698dda71575e842067515a
Diego ramon <126127427+Diegoramonn@users.noreply.github.com>
2026-05-07 12:56:59 -0300

    atu

--- SEGUNDA/aula 1/script.js
+++ SEGUNDA/aula 1/script.js
@@ -2,5 +2,20 @@ const cards = document.querySelectorAll('.card');
 
 window.addEventListener.querySelectorAll('scroll', () => {
     const trigger = window.innerHeight * 0.8;
+
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+
+        if (top < trigger) {
+            card.classList.add('show');
+        }
+    });
     
-}
+});
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
